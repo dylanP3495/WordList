a83a75f61cce0817447215cafa2bd0850f4d3b26
dylanP3495 <palafoxdylan@gmail.com>
2018-01-22 14:22:39 -0800

    k

---
+++ WordList.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class WordList here.
+ *
+ * @author Dylan
+ * @version 1.22.18
+ */
+import java.util.ArrayList;
+import java.util.List;//this is actually an interface and not a class
+import java.util.Scanner;
+import java.lang.Integer;
+public class WordList
+{
+    private ArrayList myList;
+
+    public WordList(){
+        myList.add("cat");
+        myList.add("mouse");
+        myList.add("frog");
+        myList.add("dog");
+        myList.add("dog");
+    }
+
+    public int numWordsOfLength(int len){
+        int 
+        for(int wordLength = 0;){
+
+        }
+
+    }
+
+    public void removeWordsOfLength(int len){
+
+    }
+
+}
